e6619ce2fc219f9fbc7fefc52be2e34eb5003a8e
soleil-taruto <soleil.taruto@gmail.com>
2021-02-09 12:52:30 +0900

    Backup 20210209125226

--- Rebuild.bat
+++ Rebuild.bat
@@ -3,4 +3,7 @@ C:\Factory\Tools\RDMD.exe /RM C:\app\Rico
 CALL qq
 C:\Factory\Rico\CxBuiltBatOnly.exe
 
+C:\Factory\SubTools\zip.exe /P C:\be\Web\DocRoot\Rico\Rico.zip C:\app\Rico $
+C:\Factory\Elsa\RunOnBatch.exe C:\be\Web Deploy.bat
+
 DIR C:\app\Rico
