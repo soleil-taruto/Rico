c6f3f00469115fccb91d4dd595c376c91619a609
soleil-taruto <soleil.taruto@gmail.com>
2021-03-03 09:53:11 +0900

    Backup 20210303095310

---
+++ t20210231_CopyToRepository/Claes20200001/Claes20200001/Tests/Test0001.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Charlotte.Commons;
+
+namespace Charlotte.Tests
+{
+	public class Test0001
+	{
+		public void Test01()
+		{
+			Test01_a(@"C:\Factory", @"C:\temp\Test0001_Factory");
+			Test01_a(@"C:\Dev\wb", @"C:\temp\Test0001_wb");
+		}
+
+		private void Test01_a(string rDir, string wDir)
+		{
+			SCommon.DeletePath(wDir);
+			SCommon.CreateDir(wDir);
+
+			new Program().CopyToRepository(rDir, wDir);
+		}
+	}
+}
