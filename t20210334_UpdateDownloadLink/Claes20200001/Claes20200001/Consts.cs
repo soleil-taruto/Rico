d14edf76739a3a3a1c294b5f8a70305cd8f99f5e
soleil-taruto <soleil.taruto@gmail.com>
2021-04-12 07:05:38 +0900

    Voyager 1 is 22813028132.462 kilometers away from the Sun.

---
+++ t20210334_UpdateDownloadLink/Claes20200001/Claes20200001/Consts.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Charlotte.Commons;
+
+namespace Charlotte
+{
+	public static class Consts
+	{
+		/// <summary>
+		/// 特設ページのルートDIR
+		/// </summary>
+		public const string SPEC_ROOT_DIR = @"C:\be\Web\SpecRoot";
+
+		/// <summary>
+		/// ダウンロード先のルートDIR
+		/// </summary>
+		public const string DOC_ROOT_DIR = @"C:\be\Web\DocRoot";
+
+		/// <summary>
+		/// ダウンロードリンクの開始パターン
+		/// </summary>
+		public const string LINK_START = "href=\"http://ornithopter.ccsp.mydns.jp:58946/anemoscope/";
+
+		/// <summary>
+		/// ダウンロードリンクの終了パターン
+		/// </summary>
+		public const string LINK_END = "\"";
+
+		/// <summary>
+		/// HTMLファイルのエンコーディング
+		/// </summary>
+		public static readonly Encoding HTML_ENCODING = SCommon.ENCODING_SJIS;
+	}
+}
---
+++ t20210334_UpdateDownloadLink/Claes20200001/Claes20200001/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Windows.Forms;
+using Charlotte.Commons;
+using Charlotte.Tests;
+
+namespace Charlotte
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			ProcMain.CUIMain(new Program().Main2);
+		}
+
+		private void Main2(ArgsReader ar)
+		{
+			if (ProcMain.DEBUG)
+			{
+				Main3();
+			}
+			else
+			{
+				Main4();
+			}
+			Common.OpenOutputDirIfCreated();
+		}
+
+		private void Main3()
+		{
+			// -- choose one --
+
+			Main4();
+			//new Test0001().Test01();
+			//new Test0002().Test01();
+			//new Test0003().Test01();
+
+			// --
+
+			Common.Pause();
+		}
+
+		private void Main4()
+		{
+			if (!Directory.Exists(Consts.SPEC_ROOT_DIR))
+				throw new Exception("no SPEC_ROOT_DIR");
+
+			if (!Directory.Exists(Consts.DOC_ROOT_DIR))
+				throw new Exception("no DOC_ROOT_DIR");
+
+			foreach (string htmlFile in Directory.GetFiles(Consts.SPEC_ROOT_DIR, "*.html", SearchOption.AllDirectories).Sort(SCommon.Comp))
+			{
+				Console.WriteLine("* " + htmlFile); // cout
+
+				string html = File.ReadAllText(htmlFile, Consts.HTML_ENCODING);
+				int startIndex = 0;
+
+				for (; ; )
+				{
+					Common.Enclosed enclosed = Common.GetEnclosed(html, Consts.LINK_START, Consts.LINK_END, startIndex);
+
+					if (enclosed == null)
+						break;
+
+					string path = enclosed.Inner;
+
+					if (
+						Regex.IsMatch(path, "^[/._0-9A-Za-z]+$") &&
+						SCommon.EndsWithIgnoreCase(path, ".zip")
+						)
+					{
+						Console.WriteLine("P.< " + path); // cout
+
+						string[] pathTokens = path.Split('/');
+
+						//if (pathTokens.Length != 3)
+						if (pathTokens.Length < 2)
+							throw new Exception("Bad pathTokens");
+
+						foreach (string pathToken in pathTokens)
+							if (!Regex.IsMatch(pathToken, "^[._0-9A-Za-z]+$"))
+								throw new Exception("Bad pathToken");
+
+						if (!SCommon.EndsWithIgnoreCase(pathTokens[pathTokens.Length - 1], ".zip"))
+							throw new Exception("Bad pathToken");
+
+						string relDir = Path.Combine(pathTokens.Take(pathTokens.Length - 1).ToArray());
+						string dir = Path.Combine(Consts.DOC_ROOT_DIR, relDir);
+
+						if (Directory.Exists(dir))
+						{
+							string[] files = Directory.GetFiles(dir, "*.zip");
+
+							Array.Sort(files, SCommon.CompIgnoreCase);
+
+							string file = files[files.Length - 1];
+							string relFile = SCommon.ChangeRoot(file, Consts.DOC_ROOT_DIR);
+							string pathNew = relFile.Replace('\\', '/');
+
+							Console.WriteLine("P.> " + pathNew); // cout
+
+							html = enclosed.Left + pathNew + enclosed.Right;
+						}
+					}
+					startIndex = html.Length - enclosed.AfterCloseTag.Length;
+				}
+				File.WriteAllText(htmlFile, html, Consts.HTML_ENCODING);
+			}
+		}
+	}
+}
