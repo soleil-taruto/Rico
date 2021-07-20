380f0e752231aafcf4e4ba1a8d9033f897fc49b3
soleil.taruto (cerulean.charlotte) <soleil.taruto@gmail.com>
2021-07-20 10:21:02 +0900

    Voyager 1 is 22957786932.398 kilometers away from the Sun.

--- t20210717_JSBuild/Claes20200001/Claes20200001/Ground.cs
+++ t20210717_JSBuild/Claes20200001/Claes20200001/Ground.cs
@@ -20,27 +20,41 @@ namespace Charlotte
 
 		public class HtmlFileInfo
 		{
-			public string File;
-			public string[] Lines;
+			public string FilePath;
+			public string Text;
+
+			public HtmlFileInfo(string file, string text)
+			{
+				this.FilePath = file;
+				this.Text = text;
+			}
 		}
 
 		public enum 識別子タイプ_e
 		{
 			名前空間 = 1,
-			関数,
 			変数,
+			関数,
 		}
 
 		public class TagInfo
 		{
-			public string File;
+			public string FilePath;
 			public int LineNo;
 			public string 識別子名;
 			public 識別子タイプ_e 識別子タイプ;
+
+			public TagInfo(string file, int lineNo, string 識別子名, 識別子タイプ_e 識別子タイプ)
+			{
+				this.FilePath = file;
+				this.LineNo = lineNo;
+				this.識別子名 = 識別子名;
+				this.識別子タイプ = 識別子タイプ;
+			}
 		}
 
 		public List<string> SourceLines = new List<string>();
-		public List<HtmlFileInfo> HtmlFiles = new List<HtmlFileInfo>();
 		public List<TagInfo> Tags = new List<TagInfo>();
+		public List<HtmlFileInfo> HtmlFiles = new List<HtmlFileInfo>();
 	}
 }
--- t20210717_JSBuild/Claes20200001/Claes20200001/Program.cs
+++ t20210717_JSBuild/Claes20200001/Claes20200001/Program.cs
@@ -69,8 +69,70 @@ namespace Charlotte
 
 			SCommon.DeletePath(Ground.I.TagsFile);
 
-			ReadAllSourceFile();
+			ReadSourceFiles();
+			CheckSyntax();
+			WriteTagsFile();
+			WriteHtmlFiles();
+		}
 
+		private void ReadSourceFiles()
+		{
+			foreach (string file in Directory.GetFiles(Ground.I.SourceDir, "*.js", SearchOption.AllDirectories))
+			{
+				string nameOfSpace = Path.GetFileNameWithoutExtension(file);
+				string[] lines = File.ReadAllLines(file, SCommon.ENCODING_SJIS);
+
+				Ground.I.Tags.Add(new Ground.TagInfo(file, 1, nameOfSpace, Ground.識別子タイプ_e.名前空間));
+
+				for (int index = 0; index < lines.Length; index++)
+				{
+					string line = lines[index];
+
+					line = line.Replace("@@", nameOfSpace);
+
+					Ground.I.SourceLines.Add(line);
+
+					string tLine = line;
+					tLine = tLine.Replace('\t', ' ');
+					tLine = tLine.Replace('　', ' ');
+					for (int c = 0; c < 20; c++) tLine = tLine.Replace("  ", " ");
+					tLine = tLine.Trim();
+
+					if (Regex.IsMatch(tLine, "^var [^ ]+ ?;.*$")) // ? 広域変数宣言
+					{
+						string varName = tLine.Substring(4).Split(';')[0].Trim();
+
+						Ground.I.Tags.Add(new Ground.TagInfo(file, index + 1, varName, Ground.識別子タイプ_e.変数));
+					}
+					if (Regex.IsMatch(tLine, "^function [^ ]+ ?\\(.*$")) // ? 広域関数宣言
+					{
+						string funcName = tLine.Substring(9).Split('(')[0].Trim();
+
+						Ground.I.Tags.Add(new Ground.TagInfo(file, index + 1, funcName, Ground.識別子タイプ_e.関数));
+					}
+				}
+			}
+			foreach (string file in Directory.GetFiles(Ground.I.SourceDir, "*.html", SearchOption.AllDirectories))
+			{
+				string text = File.ReadAllText(file, Encoding.UTF8);
+
+				Ground.I.HtmlFiles.Add(new Ground.HtmlFileInfo(file, text));
+			}
+		}
+
+		private void CheckSyntax()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void WriteTagsFile()
+		{
+			throw new NotImplementedException();
+		}
+
+		private void WriteHtmlFiles()
+		{
+			throw new NotImplementedException();
 		}
 	}
 }
